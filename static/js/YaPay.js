18fe51bd43304e2d328bd5cb5d633a12e7e89917
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-06 13:29:36 -0500

    Show QR from POST

--- static/js/YaPay.js
+++ static/js/YaPay.js
@@ -1,9 +1,37 @@
+$("#popupBoxOnePosition").hide();
+$("#qr").attr("src", "/static/images/loading.gif");
+
 function toggle_visibility(id) {
-    if(document.getElementById("yape").checked){
-        var e = document.getElementById(id);
-        if(e.style.display == 'block')
-            e.style.display = 'none';
-        else
-            e.style.display = 'block';
-    }
+    if($("#yape").prop("checked")){
+        if ($(id).is(":hidden")){
+            console.log("Showing QR");
+            setQr("#qr");
+        }
+
+        $(id).fadeToggle("fast");
+    } 
  }
+
+
+ function setQr(id){
+     var data = {
+         "amount": 100,
+         "companyName": "drimer",
+         "companyPhone": "993321323",
+     };
+
+     data = JSON.stringify(data)
+
+     $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/payments",
+      data: data,
+      contentType: "application/json"
+    })
+     .success(function(response) {
+         $("#qr").attr("src", response["qrData"]);
+     })
+     .fail(function(error) { alert(JSON.stringify(error))});
+
+}
+
